69bc86874d4223bb6cc33597fa4c253379a597b7
karola <k4aro0@gmail.com>
2022-04-06 16:00:05 +0200

    api test added

---
+++ backend/tests/test_api/test_encryptor.py
@@ -0,0 +1,25 @@
+import pytest
+
+
+@pytest.mark.parametrize(
+    "cipher_code,msg_text,cipher_params,result",
+    [
+        ("switch", "test", {"switch_key": "ga-de-ry-po-lu-ki"}, "tdst"),
+        ("ceasar", "test", {"shift": 1}, "uftu"),
+    ],
+)
+def test_encryptor(
+    client, cipher_code: str, msg_text: str, cipher_params: dict, result: str
+) -> None:
+    request_data = {
+        "cipher": cipher_code,
+        "text": msg_text,
+        "params": cipher_params,
+    }
+    response = client.post("/api", json=request_data)
+
+    print(response)
+
+    assert response.status_code == 200
+    assert response.json["result"] == result
+    assert response.json["result"] != msg_text
