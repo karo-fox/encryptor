39b18790faa7a73dd268116ac72d0659f5d6ea69
karola <k4aro0@gmail.com>
2022-04-06 14:48:36 +0200

    test config

--- backend/ciphers/cipher_context.py
+++ backend/ciphers/cipher_context.py
@@ -1,10 +1,6 @@
 from __future__ import annotations
 from ciphers.cipher import ICipher
 
-from ciphers.test_cipher import TestCipher
-from ciphers.ceasar_cipher import CeasarCipher
-from ciphers.switch_cipher import SwitchCipher
-
 
 class CipherContext:
     def __init__(self, cipher: ICipher) -> None:
--- backend/ciphers/test_cipher.py
+++
@@ -1,6 +0,0 @@
-from ciphers.cipher import ICipher
-
-
-class TestCipher(ICipher):
-    def encrypt(self) -> str:
-        return f"TestCipher -> text: {self.text}"
