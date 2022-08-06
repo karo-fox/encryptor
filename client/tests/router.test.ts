8704747803ebe28ed10eedda8eb290fb67dbca6c
anonymous-fox <k4aro0@gmail.com>
2022-08-06 17:54:49 +0200

    wrap up

--- client/tests/router.test.ts
+++ client/tests/router.test.ts
@@ -1,23 +1,48 @@
-import { expect, test, describe } from "vitest";
-import { render, fireEvent } from "@testing-library/vue";
+import { expect, test, describe, beforeEach, afterEach } from "vitest";
+import { render, fireEvent, cleanup } from "@testing-library/vue";
 
 import App from "@/App.vue";
 import { i18n } from "@/plugins/i18n";
 import router from "@/router";
 
-const { getByText, getByRole } = render(App, {
-  global: { plugins: [i18n, router] },
-});
-
-router.push("/");
-await router.isReady();
+afterEach(() => cleanup());
 
 describe("full app navigation", () => {
-  test("defaults to home view", () => {
+  test("routes to home view on '/'", async () => {
+    const { getByText } = render(App, {
+      global: { plugins: [i18n, router] },
+    });
+    router.push("/");
+    await router.isReady();
     getByText("Your message:");
   });
 
-  test.todo("routes to ciphers view on '/ciphers'", async () => {
-    null;
+  test("routes to ciphers view on '/ciphers'", async () => {
+    const { getByText } = render(App, {
+      global: { plugins: [i18n, router] },
+    });
+    router.push("/ciphers");
+    await router.isReady();
+    getByText("Available ciphers:");
+  });
+
+  describe("routes to cipher's descriptions on '/ciphers/...'", () => {
+    test("routes to ceasar's description on '/ciphers/ceasar", async () => {
+      const { getByText } = render(App, {
+        global: { plugins: [i18n, router] },
+      });
+      router.push("/ciphers/ceasar");
+      await router.isReady();
+      getByText("Ceasar Cipher");
+    });
+  });
+
+  test("routes to switch's description on '/ciphers/switch", async () => {
+    const { getByText } = render(App, {
+      global: { plugins: [i18n, router] },
+    });
+    router.push("/ciphers/switch");
+    await router.isReady();
+    getByText("Switch Cipher");
   });
 });
