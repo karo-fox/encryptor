01b78b66d694d5de69182f42d845d7caede27778
anonymous-fox <k4aro0@gmail.com>
2022-09-21 12:10:28 +0200

    feat(client/description): add cipher descriptions

--- client/src/components/description/SwitchDescription.vue
+++ client/src/components/description/SwitchDescription.vue
@@ -26,10 +26,14 @@ const { t } = useI18n();
     "pl": {
       "switch": "Zamiana",
       "switchKey": "Klucz zamiany",
+      "switch-desc": "Zaszyfrowanie informacji polega na zamianie litery na tę, która stoi z nią w parze, według podanego klucza. Litery, których nie ma w kluczu, są pozostawiane bez zmian",
+      "switch-key-desc": "Określa klucz, według którego zamieniane są litery"
     },
     "en": {
       "switch": "Switch",
       "switchKey": "switch key",
+      "switch-desc": "Encrypting the information involves replacing the letter with the one that stands with it in pair, according to a given key. Letters that are not in the key are left unchanged",
+      "switch-key-desc": "Specifies the key by which letters are swapped"
     }
   }
 </i18n>
